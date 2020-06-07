aea9f3b1a283d387ac7cd73b9742e876d8b87da2
broana-ait <torra.agustinignacio@gmail.com>
2020-06-07 20:50:37 -0300

    update App component for display basic tooltip

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import logo from './logo.svg';
+import Tippy from "@tippy.js/react";
+import "tippy.js/dist/tippy.css";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
+	return (
+		<div className="App">
+			<Tippy delay={1000} placement='right' content='Basic tooltip'>
+				<button>Hover</button>
+			</Tippy>
+			{/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,9 +23,9 @@ function App() {
         >
           Learn React
         </a>
-      </header>
-    </div>
-  );
+      </header> */}
+		</div>
+	);
 }
 
 export default App;
